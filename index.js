2ba0ade9b2559e09a8895a82cc498ab68c4640b6
Jean Joulia <jjeeaann2015@gmail.com>
2022-03-15 23:27:23 +0000

    hot relead

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+const electronReload = require('electron-reload')(__dirname)
+
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow();
+  const mainWindow = new BrowserWindow({
+    width: 190,
+    height: 120,
+    resizable: false,
+  });
+
   mainWindow.loadFile(path.join(__dirname, "public/index.html"));
   mainWindow.webContents.openDevTools();
 });
